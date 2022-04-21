a0b0e275ae56839f6c2702867e35236cd18bd987
HKMC\H2203737 <dmstn1812@naver.com>
2022-04-21 09:30:01 +0900

    비동기 제거

--- src/main/java/com/kwp/interfaceapi/KwpInterfaceApplication.java
+++ src/main/java/com/kwp/interfaceapi/KwpInterfaceApplication.java
@@ -7,14 +7,7 @@ import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteExcep
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-
-@EnableAsync
-@EnableCaching
-@EnableScheduling
 @EnableBatchProcessing
 @SpringBootApplication
 public class KwpInterfaceApplication {
